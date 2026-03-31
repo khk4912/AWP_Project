2f569bc593709e8ffbc8ec2f3e27e1d6b1657697
khk4912 <kosame@kosame.dev>
2026-04-01 01:14:38 +0900

    ✨ feat: Setup frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import neostandard from 'neostandard'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+const ignores = ['dist', '.**/*']
+
+export default defineConfig([
+  globalIgnores(ignores),
+  neostandard({
+    ts: true,
+    ignores,
+    filesTs: ['**/*.{ts,tsx}']
+  }),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+        tsconfigRootDir: import.meta.dirname,
+      }
+    },
+  },
+])
