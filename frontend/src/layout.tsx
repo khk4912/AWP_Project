e617338ff30b8a75e80369d6a1f2ffec76db91ed
khk4912 <kosame@kosame.dev>
2026-04-05 01:30:59 +0900

    ✨ feat: Add profile section

--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -52,14 +52,23 @@ const Logo = () => (
 export default function Layout () {
   return (
     <div className='flex bg-black min-h-screen'>
-      <aside className='min-h-screen px-4 w-60 border-r border-zinc-600 text-white'>
-        <nav className='flex flex-col py-10 gap-4'>
+      <aside className='flex min-h-screen w-60 flex-col border-r border-zinc-600 px-4 text-white'>
+        <nav className='h-full flex flex-col gap-3 py-10'>
           <Logo />
           <LayoutItem title='홈' href='/' Icon={HomeIcon} />
           <LayoutItem title='탐색' href='/search' Icon={SearchIcon} />
           <LayoutItem title='북마크' href='/bookmark' Icon={BookmarkIcon} />
           <LayoutItem title='채팅' href='/chat' Icon={ChatIcon} />
           <LayoutItem title='프로필' href='/profile' Icon={ProfileIcon} />
+          <div className='flex-1 flex flex-col justify-end'>
+            <div className='flex items-center gap-3 rounded-2xl border-2 border-zinc-800 px-4 py-2'>
+              <ProfileIcon className='w-6' />
+              <div>
+                <p className='text-sm text-zinc-300'>프로필</p>
+                <p className='text-sm text-zinc-500'>@profile</p>
+              </div>
+            </div>
+          </div>
         </nav>
       </aside>
 
