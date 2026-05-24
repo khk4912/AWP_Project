cce2dece63f5f6a789f7398cb2e5a633a1e5e6cb
khk4912 <kosame@kosame.dev>
2026-05-24 16:19:02 +0900

    🔧 chore: Remove remote font build dependency

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from 'next'
-import { Noto_Sans_KR as notoSansKrFont } from 'next/font/google'
 
 import './globals.css'
-
-const notoSansKr = notoSansKrFont({
-  // weight: ['400', '500', '600', '700'],
-  display: 'swap',
-  preload: false,
-})
 
 export const metadata: Metadata = {
   title: 'Z',
@@ -21,7 +14,7 @@ export default function RootLayout ({
 }) {
   return (
     <html lang='ko'>
-      <body className={notoSansKr.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
